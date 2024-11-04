1c60b679a06f767ecba247b18c232525d7278ff9
Airfix982 <25roter52@gmail.com>
2024-11-04 18:09:33 +0400

    fix: small fix

--- AdmissionCommittee.Api/Program.cs
+++ AdmissionCommittee.Api/Program.cs
@@ -38,9 +38,6 @@ builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-
-DotNetEnv.Env.Load();
-builder.Configuration   .AddEnvironmentVariables();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 23))));
