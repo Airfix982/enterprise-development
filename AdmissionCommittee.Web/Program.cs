c292af93021956df312ea4286c9de6bddc4516ef
Airfix982 <25roter52@gmail.com>
2024-11-16 16:01:53 +0400

    fix: small fi

--- AdmissionCommittee.Web/Program.cs
+++ AdmissionCommittee.Web/Program.cs
@@ -6,8 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5191/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddScoped<Client>(sp =>
     new Client("http://localhost:5191/", sp.GetRequiredService<HttpClient>()));
 
